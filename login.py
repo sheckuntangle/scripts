6b928ad3e55c9fe4246e36dd509e7428a8054b90
sheckuntangle <71135847+sheckuntangle@users.noreply.github.com>
2020-10-22 12:29:42 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 printf "UID: "; cat /usr/share/untangle/conf/uid &&
 printf "Hostname: "; hostname &&
-printf "Version: "; dpkg -l | awk '{print $2 "  :  " $3  }' | grep untangle-vm
+printf "Version: "; dpkg -l | awk '{print $2 "  :  " $3  }' | grep untangle-vm | cut -b 17-
 printf "Serial: "; cat /sys/devices/virtual/dmi/id/product_serial
-printf "OpenVPN Certificate: "; openssl x509 -text -in /usr/share/untangle/settings/openvpn/server.crt | grep "Sig"
+printf "OpenVPN Certificate: "; openssl x509 -text -in /usr/share/untangle/settings/openvpn/server.crt | grep -m 1 "Signature Algorithm:" | awk '{print $3}'
