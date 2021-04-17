bff48d9773a1874e3a3b3b936d5a5c8d24396e06
sheckuntangle <71135847+sheckuntangle@users.noreply.github.com>
2021-04-17 16:57:20 -0400

    Update ut-qemu.py

--- ut-qemu.py
+++ ut-qemu.py
@@ -1,4 +1,5 @@
 cp /etc/nano/sources.list > /etc/nano/sources.list.old
+touch /etc/nano/sources.list
 echo 'deb http://deb.debian.org/debian buster main contrib non-free' >> /etc/nano/sources.list
 echo 'deb-src http://deb.debian.org/debian buster main contrib non-free' >> /etc/nano/sources.list
 apt-get update
