a66a1f3a36a6c584554e8d793a20d8848f3e9ea0
sheckuntangle <71135847+sheckuntangle@users.noreply.github.com>
2021-04-17 16:51:13 -0400

    Add files via upload

---
+++ ut-qemu.py
@@ -0,0 +1,7 @@
+cp /etc/nano/sources.list > /etc/nano/sources.list.old
+echo 'deb http://deb.debian.org/debian buster main contrib non-free' >> /etc/nano/sources.list
+echo 'deb-src http://deb.debian.org/debian buster main contrib non-free' >> /etc/nano/sources.list
+apt-get update
+apt-get install qemu-guest-agent
+rm /etc/nano/sources.list 
+mv /etc/nano/sources.list.old /etc/nano/sources.list
