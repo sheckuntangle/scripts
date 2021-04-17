cd7a4bdb237b1e90f0f6604f72a024ec906fac5b
sheckuntangle <71135847+sheckuntangle@users.noreply.github.com>
2021-04-17 16:58:55 -0400

    Update ut-qemu.py

--- ut-qemu.py
+++ ut-qemu.py
@@ -1,9 +1,9 @@
-cp /etc/nano/sources.list > /etc/nano/sources.list.old
-touch /etc/nano/sources.list
-echo 'deb http://deb.debian.org/debian buster main contrib non-free' >> /etc/nano/sources.list
-echo 'deb-src http://deb.debian.org/debian buster main contrib non-free' >> /etc/nano/sources.list
+cp /etc/apt/sources.list > /etc/apt/sources.list.old
+touch /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian buster main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian buster main contrib non-free' >> /etc/apt/sources.list
 apt-get update
 apt-get install qemu-guest-agent
-rm /etc/nano/sources.list 
-mv /etc/nano/sources.list.old /etc/nano/sources.list
+rm /etc/apt/sources.list 
+mv /etc/apt/sources.list.old /etc/apt/sources.list
 rm /usr/share/untangle/conf/uid
