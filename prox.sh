e182471365fe2c398897089f96ff32b04402ebf4
sheckuntangle <71135847+sheckuntangle@users.noreply.github.com>
2021-10-12 14:25:18 -0400

    Update prox.sh

--- prox.sh
+++ prox.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#mv pve-enterprise to .old
-mv /etc/apt/sources.list.d/pve-enterprise.list...http://download.proxmox.com/debian/pve buster pve-no-subscription' >> /etc/apt/sources.list
+# mv pve-enterprise to .old
+mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.old
 
-#update
+# uncomment buster line for proxmox 6, uncomment bullseye line for proxmox 7
+#echo 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription' >> /etc/apt/sources.list
+#echo 'deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription' >> /etc/apt/sources.list
+
+# update
 apt-get update
 
-#install ifupdown2 (allows networks to be created without rebooting
+# install ifupdown2 (allows networks to be created without rebooting
 apt-get install ifupdown2 -y
 
-#upgrade and dist-upgrade
+# upgrade and dist-upgrade
 apt-get upgrade -y && apt-get dist-upgrade -y
